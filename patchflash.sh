82642c218c09863fda21f0733b2e11d792b0cfc5
schrotthaufen <dont@contact.me>
2013-01-09 02:30:18 +0100

    Use sudo if available

--- patchflash.sh
+++ patchflash.sh
@@ -2,10 +2,17 @@
 # Thanks bla for the awesome dd trick :)
 
 cd /tmp
-cp /usr/lib/mozilla/plugins/libflashplayer.so  .
+cp /usr/lib/mozilla/plugins/libflashplayer.so .
 cp libflashplayer.so libflashplayer.so.$(date +%s)
+
 for i in $(objdump -d libflashplayer.so -Mintel |grep unlink | grep "^ "|cut -d: -f1 |tr -d ':') ; do
 
         echo -n $'\x90\x90\x90\x90\x90' | dd if=/proc/self/fd/0 of=libflashplayer.so conv=notrunc bs=1 seek=$((16#$i));
 done
-su -c 'cp libflashplayer.so /usr/lib/mozilla/plugins/libflashplayer.so'
+
+which sudo > /dev/null
+if [ $? -eq 0 ]; then
+	sudo cp libflashplayer.so /usr/lib/mozilla/plugins/libflashplayer.so
+else
+	su -c 'cp libflashplayer.so /usr/lib/mozilla/plugins/libflashplayer.so'
+fi
