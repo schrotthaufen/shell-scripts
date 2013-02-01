b317a1907402575c25df2e0ffb6adf679066ae37
schrotthaufen <dont@contact.me>
2013-02-01 20:09:52 +0100

    nasmit.sh can now read assembly from and write sc to a file

--- nasmit.sh
+++ nasmit.sh
@@ -1,28 +1,76 @@
 #!/bin/bash
 # The author does not care about a license
 
-if [ $# -ne 1 ]; then
-  echo "Usage: ${0} {16|32|64}" 1>&2
-  exit 1
+usage() {
+  echo "Usage: ${0} [-b {16|32|64}] [-f infile] [-o outfile]" 1>&2
+}
+
+if [ "${#}" -lt 1 ]; then
+	usage
+	exit 1;
 fi
 
-echo "End input with empty line" 1>&2
+INFILE="/tmp/nasmit.asm"
+OUTFILE="/dev/stdout"
 
-if [ -e /tmp/nasmit.asm ]; then
-  rm /tmp/nasmit.asm
-fi
-
-CODE="BITS ${1}"
-
-until [ "${CODE}" == "" ]; do
-  echo $CODE >> /tmp/nasmit.asm
-  read CODE
+while getopts ":b:f:o:h" opt; do
+	case "${opt}" in
+		b) BITS="${OPTARG}";
+			case "${BITS}" in
+				16) break;
+					;;
+				32) break;
+					;;
+				64) break;
+					;;
+				*) echo "Only 16, 32 or 64 are valid values" 1>&2;
+					exit 1;
+					;;
+				esac
+			;;
+		f) INFILE="${OPTARG}";
+			fflag=true;
+			if [ ! -e "${INFILE}" ]; then
+				echo "File not found: ${INFILE}" 1>&2;
+				exit 1;
+			fi
+			;;
+		o) OUTFILE="${OPTARG}";
+			;;
+		h) usage;
+			exit 1;
+			;;
+		\?) usage;
+			exit 1;
+			;;
+		*) usage;
+			exit 1;
+			;;
+	esac
 done
 
-nasm -f bin -o /dev/stdout /tmp/nasmit.asm | \
+shift $(( OPTIND - 1 ));
+
+if [[ "${INFILE}" == "/tmp/nasmit.asm" && ${fflag} != true ]]; then
+
+	echo "End input with empty line" 1>&2
+	echo "You will find your assemby in /tmp/nasmit.asm" 1>&2
+
+	if [ -e /tmp/nasmit.asm ]; then
+  	rm /tmp/nasmit.asm
+	fi
+
+	CODE="BITS ${BITS}"
+
+	until [ "${CODE}" == "" ]; do
+  	echo $CODE >> /tmp/nasmit.asm
+  	read CODE
+	done
+fi
+
+nasm -f bin -o /dev/stdout "${INFILE}" | \
 	xxd -p | \
 	tr -d '\n' | \
-	sed -r "s/(..)/\\\\x\1/g"
+	sed -r "s/(..)/\\\\x\1/g" > "${OUTFILE}"
 
 echo ""
-rm /tmp/nasmit.asm
